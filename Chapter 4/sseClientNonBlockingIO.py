0e69e7e2b8ad92cea866cde3a45a593389c3ac3a
Nicolas Bohorquez <nicolas.bohorquez@mailbox.org>
2024-12-04 16:03:03 -0500

    feat: Adds async sse non-blocking example

--- Chapter 4/sseClientNonBlockingIO.py
+++ Chapter 4/sseClientNonBlockingIO.py
@@ -1,16 +1,18 @@
 import json
 import queue
 import asyncio
+import aiofiles
 from aiosseclient import aiosseclient
 
 max_vals = 10
 vals = queue.Queue(maxsize=max_vals)  
+fetch_done = False
 
 async def fetcher():
     while True:
         try:
             async for item in aiosseclient('https://stream.wikimedia.org/v2/stream/recentchange'):
-                vals.put(item, block=False)
+                vals.put(json.loads(item.data), block=False)
                 await asyncio.sleep(1)
         except queue.Full:
             print("Queue is full")
@@ -20,6 +22,7 @@ async def monitor():
     while True:
         curr_len = vals.qsize()
         if curr_len >= max_vals:
+            fetch_done = True
             return True
         else:
             print("Queue size:",curr_len)
@@ -30,12 +33,12 @@ async def serializer():
         try:
             item = vals.get_nowait()
             print("item read, Wiki edited by:", item["user"])
-            f = open(f'./tmp/${item["id"]}.json', "a")
-            json.dump(item, f)
-            f.close()
+            async with aiofiles.open(f'./tmp/${item["id"]}.json', mode='w') as f:
+                await f.write(json.dumps(item))
         except queue.Empty:
             print("Queue is empty")
-            return True
+            if fetch_done == True:
+                return True
         await asyncio.sleep(1)
         
 async def main():
