5a6bc6935dbd1280910b17f9170f6e8e3b21c2bb
Nicolas Bohorquez <nicolas.bohorquez@mailbox.org>
2025-02-19 17:05:18 -0500

    chore: clean prev examples

--- Chapter 5/synch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gunicorn -w 1 --name ch5 --threads 1 --timeout 1 --backlog 0 --bind 127.0.0.1:8080 app_flask:app
-#curl -X POST http://localhost:8080/classify?id=112345&image=@/img/1.jpg -H 'Content-Type: multipart/form-data'
