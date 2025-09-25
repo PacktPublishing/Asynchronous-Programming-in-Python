b48e1c666038b7598fe5bef1eee2f6ffac6e44cf
Nicolas Bohorquez <nicolas.bohorquez@mailbox.org>
2025-09-24 19:09:58 -0500

    chore: updated code for clarity

--- Chapter 3/piTime.py
+++ Chapter 3/piTime.py
@@ -1,29 +1,27 @@
 import time
 
 def pi_approximation(terms):
-    numerator = 4.0
     denominator = 1.0
     sign = 1.0
-    pi_approx = 0.0
+    result = 0.0
 
     for _ in range(terms):
-        pi_approx += sign * (numerator / denominator)
+        result += sign * (4.0 / denominator)
         sign *= -1
         denominator += 2
 
-    return pi_approx
+    return result
 
 def pi_approximation_generator(terms):
-    numerator = 4.0
     denominator = 1.0
     sign = 1.0
-    pi_approx = 0.0
+    result = 0.0
 
     for _ in range(terms):
-        pi_approx += sign * (numerator / denominator)
+        result += sign * (4.0 / denominator)
         sign *= -1
         denominator += 2
-        yield pi_approx
+        yield result
 
 if __name__ == '__main__':
     num_iters = 1000000        
